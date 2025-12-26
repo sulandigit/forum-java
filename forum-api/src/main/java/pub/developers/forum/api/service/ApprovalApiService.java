f6c3d624f0de364bd5d3216d034e651c2ee05e01
QoderAI <215938697+qoderai[bot]@users.noreply.github.com>
2025-12-26 09:39:48 +0000

    docs: add english comments to ApprovalApiService
    Co-authored-by: landi su <>

--- forum-api/src/main/java/pub/developers/forum/api/service/ApprovalApiService.java
+++ forum-api/src/main/java/pub/developers/forum/api/service/ApprovalApiService.java
@@ -3,16 +3,36 @@ package pub.developers.forum.api.service;
 import pub.developers.forum.api.model.ResultModel;
 
 /**
+ * Approval API Service interface for managing post approvals
+ * 
  * @author Qiangqiang.Bian
  * @create 2020/12/1
  * @desc
  **/
 public interface ApprovalApiService {
 
+    /**
+     * Create an approval for a post
+     * 
+     * @param postsId the ID of the post to approve
+     * @return ResultModel containing the approval ID
+     */
     ResultModel<Long> create(Long postsId);
 
+    /**
+     * Delete an approval for a post
+     * 
+     * @param postsId the ID of the post to remove approval from
+     * @return ResultModel containing the deleted approval ID
+     */
     ResultModel<Long> delete(Long postsId);
 
+    /**
+     * Check if a post has been approved
+     * 
+     * @param postsId the ID of the post to check
+     * @return ResultModel containing true if the post has approval, false otherwise
+     */
     ResultModel<Boolean> hasApproval(Long postsId);
 
 }
