460b675b77899df466b47957e633f03daf4d8486
QoderAI <215938697+qoderai[bot]@users.noreply.github.com>
2025-12-31 08:47:55 +0000

    docs: add english comments to ApprovalApiServiceImpl
    Co-authored-by: landi su <>

--- forum-facade/src/main/java/pub/developers/forum/facade/impl/ApprovalApiServiceImpl.java
+++ forum-facade/src/main/java/pub/developers/forum/facade/impl/ApprovalApiServiceImpl.java
@@ -9,9 +9,11 @@ import pub.developers.forum.facade.support.ResultModelUtil;
 import javax.annotation.Resource;
 
 /**
+ * Implementation of ApprovalApiService
+ * 
  * @author Qiangqiang.Bian
  * @create 2020/12/1
- * @desc
+ * @desc Approval API service implementation for managing post approvals
  **/
 @Service
 public class ApprovalApiServiceImpl implements ApprovalApiService {
@@ -19,18 +21,39 @@ public class ApprovalApiServiceImpl implements ApprovalApiService {
     @Resource
     private ApprovalManager approvalManager;
 
+    /**
+     * Create an approval for a post
+     * 
+     * @param postsId the ID of the post to approve
+     * @return result model containing the approval ID
+     */
     @Override
     public ResultModel<Long> create(Long postsId) {
+        // Delegate to approval manager to create approval
         return ResultModelUtil.success(approvalManager.create(postsId));
     }
 
+    /**
+     * Delete an approval for a post
+     * 
+     * @param postsId the ID of the post whose approval should be deleted
+     * @return result model containing the deleted approval ID
+     */
     @Override
     public ResultModel<Long> delete(Long postsId) {
+        // Delegate to approval manager to delete approval
         return ResultModelUtil.success(approvalManager.delete(postsId));
     }
 
+    /**
+     * Check if a post has been approved
+     * 
+     * @param postsId the ID of the post to check
+     * @return result model containing true if post is approved, false otherwise
+     */
     @Override
     public ResultModel<Boolean> hasApproval(Long postsId) {
+        // Delegate to approval manager to check approval status
         return ResultModelUtil.success(approvalManager.hasApproval(postsId));
     }
 
